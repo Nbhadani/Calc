cb35def18a8973ba16cb07d12a648b284852858d
nbhadani <nehal.bhadani09@gmail.com>
2019-12-11 18:40:22 +0000

    Uploading Calculator2.java homewrok

---
+++ Calculator2.java
@@ -0,0 +1,76 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.sql.SQLOutput;
+
+public class Calculator2 {
+
+
+    static int a;
+    static int b = 20;
+    static int c;
+    static int d = 10;
+    static int e;
+    static int f = 30;
+    static int g;
+    static int h = 40;
+
+    public static void subtraction(int a, int b) {
+       System.out.println(" ");
+       System.out.print("Subtraction of " + a + " and " + b + " = ");
+       System.out.println(a - b);
+        }
+
+    public static void addition(int c, int d) {
+        System.out.println( " ");
+        System.out.print("Addition of " + c + " and " + d + " = ");
+        System.out.println(c + d);
+       }
+
+    public static void multiplication(int e, int f) {
+        System.out.println( " ");
+        System.out.print("Multiplication of " + e + " and " + f + " = ");
+        System.out.println(e * f);
+    }
+
+    public static void division(int g, int h) {
+        System.out.println( " ");
+        System.out.print("Division of " + g + " by " + h + " = ");
+        System.out.println(g / h);
+    }
+    public static void main(String [] args) {
+            String company = "PRINT HOMEWORK";
+            System.out.println(company);
+
+
+            addition (  50, 25 );
+            System.out.println(d);
+
+            subtraction (  50, 25 );
+            System.out.println(b);
+            System.out.println( " ");
+
+            multiplication (  80, 40 );
+            System.out.println(f);
+            System.out.println( " ");
+
+            division (  80, 10 );
+            System.out.println(h);
+            System.out.println( " ");
+
+        }
+
+
+    }
+
+   /* public static void main(String [] args) {
+        String company = "TFL";
+        System.out.println(company);
+        subtraction (  50, 25 );
+        System.out.println(b);
+    }*/
+
+
+
+
+
+
